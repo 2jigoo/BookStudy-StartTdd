83628ea34fcccbc4af42e4280b9c4abcd11e0189
2jigoo <2jigoo@naver.com>
2023-08-06 21:59:36 +0900

    #2 암호 검사기 - (1) 모든 규칙을 충족하는 경우

---
+++ study-tdd/src/test/java/chap02/PasswordStrengthMeter.java
@@ -0,0 +1,7 @@
+package chap02;
+
+public class PasswordStrengthMeter {
+    public PasswordStrength meter(String s) {
+        return PasswordStrength.STRONG;
+    }
+}
---
+++ study-tdd/src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -0,0 +1,20 @@
+package chap02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PasswordStrengthMeterTest {
+
+    @Test
+    void meetsAllCriteriaThenStrong() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+        PasswordStrength result = meter.meter("ab12!@AB");
+        assertEquals(PasswordStrength.STRONG, result);
+
+        PasswordStrength result2 = meter.meter("abc1!Add");
+        assertEquals(PasswordStrength.STRONG, result2);
+    }
+    
+}
