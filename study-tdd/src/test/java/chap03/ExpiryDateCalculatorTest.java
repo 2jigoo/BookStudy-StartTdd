6dce02ed082f607054956b714a7021120f704704
2jigoo <2jigoo@naver.com>
2023-08-15 17:30:55 +0900

    #10 서비스 만료일 계산기 - refactor

--- study-tdd/src/test/java/chap03/ExpiryDateCalculatorTest.java
+++ study-tdd/src/test/java/chap03/ExpiryDateCalculatorTest.java
@@ -10,21 +10,20 @@ class ExpiryDateCalculatorTest {
 
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨() {
-        LocalDate billingDate = LocalDate.of(2019, 3, 1);
-        int payAmount = 10_000;
+        assertExpiryDate(
+                LocalDate.of(2019, 3, 1), 10_000,
+                LocalDate.of(2019, 4, 1));
 
+        assertExpiryDate(
+                LocalDate.of(2019, 5, 5), 10_000,
+                LocalDate.of(2019, 6, 5));
+    }
+    
+    private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
         ExpiryDateCalculator calculator = new ExpiryDateCalculator();
-        LocalDate expiryDate = calculator.calculateExpiryDate(billingDate, payAmount);
+        LocalDate actualExpiryDate = calculator.calculateExpiryDate(billingDate, payAmount);
 
-        assertEquals(LocalDate.of(2019, 4, 1), expiryDate);
-
-        LocalDate billingDate2 = LocalDate.of(2019, 5, 5);
-        int payAmount2 = 10_000;
-
-        ExpiryDateCalculator calculator2 = new ExpiryDateCalculator();
-        LocalDate expiryDate2 = calculator.calculateExpiryDate(billingDate2, payAmount2);
-
-        assertEquals(LocalDate.of(2019, 6, 5), expiryDate2);
+        assertEquals(expectedExpiryDate, actualExpiryDate);
     }
 
 }
