222f03bdcaf7f85a50c35bbf3b63bc2bad4d6cb5
Jonghai <ljh7859@naver.com>
2023-08-16 02:16:02 +0900

    chap03: 첫번째 테스트

---
+++ src/main/java/chap02/PasswordStrengthMeter.java
@@ -0,0 +1,21 @@
+package chap02;
+
+public class PasswordStrengthMeter {
+    public PasswordStrength meter(String s){
+        if(s.length()<8){
+            return PasswordStrength.NORMAL;
+        }
+        boolean containsNum = meetsContainingNumberCriteria(s);
+        if (!containsNum) return PasswordStrength.NORMAL;
+        return PasswordStrength.STRONG;
+    }
+
+    private boolean meetsContainingNumberCriteria(String s){
+        for(char ch : s.toCharArray()) {
+            if (ch>= '0' && ch <='9'){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/chap03/ExpriyDateCalculator.java
@@ -0,0 +1,11 @@
+package chap03;
+
+import java.time.LocalDate;
+
+public class ExpriyDateCalculator {
+
+    public LocalDate calculateExpriyDate(LocalDate billingDate, int payAmount){
+        return LocalDate.of(2019,4,1);
+    }
+
+}
