897d385420e9897985748d1f18e17c39533061d1
JongHa11 <ljh7859@clipsoft.co.kr>
2023-08-16 10:37:33 +0900

    #11 chap03: 예외상황처리

--- src/test/java/chap03/ExpriyDateCalculatorTest.java
+++ src/test/java/chap03/ExpriyDateCalculatorTest.java
@@ -11,6 +11,13 @@ class ExpriyDateCalculatorTest {
     void 만원_납부하면_한달_뒤가_만료일이_됨(){
         assertExpriyDate(LocalDate.of(2019,3,1), 10_000, LocalDate.of(2019,4,1));
         assertExpriyDate(LocalDate.of(2019,5,5), 10_000, LocalDate.of(2019,6,5));
+        assertExpriyDate(LocalDate.of(2019,5,31), 10_000, LocalDate.of(2019,6,30));
+        assertExpriyDate(LocalDate.of(2020,1,31), 10_000, LocalDate.of(2020,2,29));
+    }
+
+    @Test
+    void 납부일과_힌달_뒤_일자가_같지_않음(){
+        assertExpriyDate(LocalDate.of(2019,1,31),10_000, LocalDate.of(2019,2,28));
     }
 
     private void assertExpriyDate(LocalDate billingDate, int payAmount, LocalDate expectedExpriyDate){
