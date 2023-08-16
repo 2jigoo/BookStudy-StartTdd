93e66a795a9eab99d69b80a745005b624fc4905b
Jonghai <ljh7859@naver.com>
2023-08-17 00:22:46 +0900

    #11 chap03:코드정리:상수를 변수로

--- src/test/java/chap03/ExpriyDateCalculator.java
+++ src/test/java/chap03/ExpriyDateCalculator.java
@@ -5,12 +5,13 @@ import java.time.LocalDate;
 public class ExpriyDateCalculator {
 
     public LocalDate calculateExpriyDate(PayData payData) {
+        int addedMonths = 1;
         if (payData.getFirstBillingDate() != null) {
-            LocalDate candidateExp = payData.getBillingData().plusMonths(1);
+            LocalDate candidateExp = payData.getBillingData().plusMonths(addedMonths);
             if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()){
                 return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
             }
         }
-        return payData.getBillingData().plusMonths(1);
+        return payData.getBillingData().plusMonths(addedMonths);
     }
 }
