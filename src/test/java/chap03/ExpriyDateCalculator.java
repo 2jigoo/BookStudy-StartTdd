8d9e3b12c49890adbbe7e96292062241669529d1
JongHa11 <ljh7859@clipsoft.co.kr>
2023-08-16 10:28:46 +0900

    chap03: 만원 납부하면 한달 뒤가 만료일이 됨 코드 정리

--- src/test/java/chap03/ExpriyDateCalculator.java
+++ src/test/java/chap03/ExpriyDateCalculator.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class ExpriyDateCalculator {
 
     public LocalDate calculateExpriyDate(LocalDate billingDate, int payAmount){
-        return LocalDate.of(2019,4,1);
+        return billingDate.plusMonths(1);
     }
 
 }
--- src/test/java/chap03/ExpriyDateCalculatorTest.java
+++ src/test/java/chap03/ExpriyDateCalculatorTest.java
@@ -9,13 +9,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class ExpriyDateCalculatorTest {
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨(){
-        LocalDate billingDate = LocalDate.of(2019,3,1);
-        int payAmount = 10_000;
+        assertExpriyDate(LocalDate.of(2019,3,1), 10_000, LocalDate.of(2019,4,1));
+        assertExpriyDate(LocalDate.of(2019,5,5), 10_000, LocalDate.of(2019,6,5));
+    }
 
+    private void assertExpriyDate(LocalDate billingDate, int payAmount, LocalDate expectedExpriyDate){
         ExpriyDateCalculator cal = new ExpriyDateCalculator();
-        LocalDate expriyDate = cal.calculateExpriyDate(billingDate,payAmount);
-
-        assertEquals(LocalDate.of(2019,4,1), expriyDate);
+        LocalDate realExpriyDate = cal.calculateExpriyDate(billingDate,payAmount);
+        assertEquals(expectedExpriyDate, realExpriyDate);
     }
 
 }
