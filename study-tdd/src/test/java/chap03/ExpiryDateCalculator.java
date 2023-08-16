81c428e9d1a78f71b4e7edffdebcc8e464ba3e9b
2jigoo <2jigoo@naver.com>
2023-08-16 23:48:08 +0900

    #10 만료일 계산기 서비스 - refactor

--- study-tdd/src/test/java/chap03/ExpiryDateCalculator.java
+++ study-tdd/src/test/java/chap03/ExpiryDateCalculator.java
@@ -8,14 +8,16 @@ public class ExpiryDateCalculator {
     }
 
     public LocalDate calculateExpiryDate(PayData payData) {
+        int addedMonths = 1;
+
         if (payData.getFirstBillingDate() != null) {
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(1);
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
             if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
                 return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
             }
         }
 
-        return payData.getBillingDate().plusMonths(1);
+        return payData.getBillingDate().plusMonths(addedMonths);
     }
     
 }
