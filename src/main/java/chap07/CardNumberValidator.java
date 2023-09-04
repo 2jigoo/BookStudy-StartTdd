e84a020e11e92388b9fcf69cf2b72db49ac3b2d0
Jonghai <ljh7859@naver.com>
2023-09-05 02:00:38 +0900

    #20 chap07:대역의 필요성

---
+++ src/main/java/chap07/CardNumberValidator.java
@@ -0,0 +1,30 @@
+package chap07;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class CardNumberValidator {
+    public CardValidity validate(String cardNumber){
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://some-external-pg.com/card"))
+                .header("Content-Type", "text/plain")
+                .POST(HttpRequest.BodyPublishers.ofString(cardNumber))
+                .build();
+        try{
+            HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
+            switch (response.body()){
+                case "ok": return CardValidity.VALID;
+                case "bad": return CardValidity.INVALID;
+                case "expired": return CardValidity.EXPIRED;
+                case "theft" : return CardValidty.THEFT;
+                default: return CardValidity.UNKNOWN;
+            }
+        }catch (IOException | InterruptedException e){
+            return CardValidity.ERROR;
+        }
+    }
+}
---
+++ src/test/java/chap07/AutoDebitRegisterTest.java
@@ -0,0 +1,24 @@
+package chap07;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutoDebitRegisterTest {
+    private AutoDebitRegister register;
+    @BeforeEach
+    void setUp() {
+        CardNumberValidator validator = new CardNumberValidator();
+        AutoDebitInfoRepository repository = new JpaAutoDebitInfoRepository();
+        register = new AutoDebitRegister(validator,repository);
+    }
+
+    @Test
+    void validCard(){
+        //업체에서 받은 테스트용 유효한 카드번호 사용
+        AutoDebitReq req = new AutoDebitReq("user1", "1234567890123456");
+        RegisterResult result = this.register.register(req);
+        assertEquals(THEFT, result.getValidty());
+    }
+}
