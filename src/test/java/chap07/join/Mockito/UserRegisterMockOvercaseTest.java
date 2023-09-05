c964cd7ccf395a99bd11ee29f60bde2ddd587cbe
Jonghai <ljh7859@naver.com>
2023-09-05 21:33:28 +0900

    #20 chap07:모의 객체를 과하게 사용하지 않기

---
+++ src/test/java/chap07/join/Mockito/UserRegisterMockOvercaseTest.java
@@ -0,0 +1,49 @@
+package chap07.join.Mockito;
+
+import chap07.join.UserRegister;
+import chap07.join.idcheck.MemoryUserRepository;
+import chap07.join.idcheck.User;
+import chap07.join.idcheck.UserRepository;
+import chap07.join.passwordcheck.WeakPasswordChecker;
+import chap07.join.spy.EmailNotifier;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserRegisterMockOvercaseTest {
+    private UserRegister userRegister;
+    private UserRepository mockRepository = Mockito.mock(UserRepository.class);
+    private WeakPasswordChecker mockPasswordChecker = Mockito.mock(WeakPasswordChecker.class);
+    private EmailNotifier mockEmailNotifier = Mockito.mock(EmailNotifier.class);
+    private MemoryUserRepository fakeRepository = new MemoryUserRepository();
+
+    @BeforeEach
+    void setUp(){
+        userRegister = new UserRegister(mockPasswordChecker,mockRepository,mockEmailNotifier);
+    }
+
+    @Test
+    void noDupId_RegisterSuccess(){
+        userRegister.register("id","pw","email");
+
+        ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
+        BDDMockito.then(mockRepository).should().save(captor.capture());
+
+        User savedUser = captor.getValue();
+        assertEquals("id", savedUser.getId());
+        assertEquals("email",savedUser.getEmail());
+    }
+
+    @Test
+    void 같은_ID가_없으면_가입(){
+        userRegister.register("id","pw","email");
+
+        User savedUser = fakeRepository.findById("id");
+        assertEquals("id",savedUser.getId());
+        assertEquals("email",savedUser.getEmail());
+    }
+}
