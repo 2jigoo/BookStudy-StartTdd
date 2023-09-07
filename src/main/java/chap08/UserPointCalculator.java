c5530b7baed51140658671e11f181168f1bce20e
Jonghai <ljh7859@naver.com>
2023-09-08 01:12:24 +0900

    #23 chap09:실행 시점에 따라 달라지는 결과

---
+++ src/main/java/chap08/UserPointCalculator.java
@@ -0,0 +1,33 @@
+package chap08;
+
+
+import jdk.jshell.JShell;
+
+import java.time.LocalDate;
+
+public class UserPointCalculator {
+    private SubscriptionDao subscriptionDao;;
+    private ProductDao productDao;
+
+    public UserPointCalculator(SubscriptionDao subscriptionDao, ProductDao productDao){
+        this.subscriptionDao = subscriptionDao;
+        this.productDao = productDao;
+    }
+
+    public int calculatePoint(User u){
+        Subscription s = subscriptionDao.selectByUser(u.getId());
+        if (s == null) throw new NoSuchFieldException();
+        Product p = productDao.selectById(s.getProductId());
+        LocalDate now = LocalDate.now();
+        int point = 0;
+        if (s.isFinished(now)){
+            point += p.getDefaultPoint();
+        }else {
+            point += p.getDefaultPoint();
+        }
+        if (s.getGrade() == GOLD){
+            point += 100;
+        }
+        return point;
+    }
+}
